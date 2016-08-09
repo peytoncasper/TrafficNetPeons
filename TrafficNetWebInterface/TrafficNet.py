ba50410acbea111b61aecbc0cf6af6477dbcd966
Peyton Casper <peytoncas@gmail.com>
2016-08-09 12:45:43 -0500

    Added updates for the web ui

--- TrafficNetWebInterface/TrafficNet.py
+++ TrafficNetWebInterface/TrafficNet.py
@@ -19,12 +19,57 @@ def validate_login_camera_url():
     with open('config.json') as data_file:
         data = json.load(data_file)
         if username == data["username"] and password == data["password"]:
-            return socket.gethostbyname(socket.gethostname())
+            return json.dumps({
+                "ipAddress":socket.gethostbyname(socket.gethostname()),
+                "title": data["title"],
+                "description": data["description"],
+                "direction": ""
+            })
         else:
             return ""
 
+@app.route('/get_title_description')
+def get_title_description():
+    username = request.args.get('username')
+    password = request.args.get('password')
 
+    with open('config.json') as data_file:
+        data = json.load(data_file)
+        if username == data["username"] and password == data["password"]:
+            return json.dumps({
+                "title": data["title"],
+                "description": data["description"],
+            })
+        else:
+            return ""
 
+@app.route('/save_settings')
+def save_settings():
+    username = request.args.get('username')
+    password = request.args.get('password')
+    title = request.args.get('title')
+    description = request.args.get('description')
+
+    authenticated = False
+
+    with open('config.json') as data_file:
+        data = json.load(data_file)
+        if username == data["username"] and password == data["password"]:
+            authenticated = True
+
+    if authenticated:
+        with open('config.json', 'w') as data_file:
+
+            data["title"] = title
+            data["description"] = description
+            data_file.write(json.dumps(data))
+
+            return json.dumps({
+                "title": data["title"],
+                "description": data["description"],
+            })
+    else:
+        return ""
 
 @app.route('/step_right')
 def step_right():
@@ -32,33 +77,35 @@ def step_right():
     username = request.args.get('username')
     password = request.args.get('password')
 
-    pins = {22,23,24,25,26,27,28,29}
-    # for pin in pins:
-        # RPIO.setup(pin, RPIO.OUT)
-        # RPIO.output(pin, False)
-    sequence = {{1, 0, 0, 0},
-                {1, 1, 0, 0},
-                {0, 1, 0, 0},
-                {0, 1, 1, 0},
-                {0, 0, 1, 0},
-                {0, 0, 1, 1},
-                {0, 0, 0, 1},
-                {1, 0, 0, 1}};
+    with open('config.json') as data_file:
+        data = json.load(data_file)
+        if username == data["username"] and password == data["password"]:
+            pins = {22,23,24,25,26,27,28,29}
+            # for pin in pins:
+                # RPIO.setup(pin, RPIO.OUT)
+                # RPIO.output(pin, False)
+            sequence = {{1, 0, 0, 0},
+                        {1, 1, 0, 0},
+                        {0, 1, 0, 0},
+                        {0, 1, 1, 0},
+                        {0, 0, 1, 0},
+                        {0, 0, 1, 1},
+                        {0, 0, 0, 1},
+                        {1, 0, 0, 1}};
 
-    for i in range(0, 50):
-        for halfstep in range(0, 8):
-            for pin in range(0, 4):
-                # RPIO.output(pins[pin], sequence[halfstep][pin])
-                print "Cycle"
-            # delay(1)
+            for i in range(0, 50):
+                for halfstep in range(0, 8):
+                    for pin in range(0, 4):
+                        # RPIO.output(pins[pin], sequence[halfstep][pin])
+                        print "Cycle"
+                    # delay(1)
 
-    for pin in pins:
-        # RPIO.output(pins[pin], 0)
-        print "Setting pin low"
+            for pin in pins:
+                # RPIO.output(pins[pin], 0)
+                print "Setting pin low"
+        else:
+            return ""
 
-
-
-    return ""
 def step_left():
     return ""
 def step_forward():
