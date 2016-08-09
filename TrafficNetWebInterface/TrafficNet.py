9b8584b5ce4f7717e29ba081a6316128f2fca776
Peyton Casper <peytoncas@gmail.com>
2016-08-09 16:08:24 -0500

    Updates to add config horizontal and vertical settings

--- TrafficNetWebInterface/TrafficNet.py
+++ TrafficNetWebInterface/TrafficNet.py
@@ -110,12 +110,137 @@ def step_right():
                 print "Setting pin low"
         else:
             return ""
+    with open('config.json', 'w') as data_file:
+        data["horizontal_orientation"] = int(data["horizontal_orientation"]) + 1
+        data_file.write(json.dumps(data))
+
+        return ""
 
 def step_left():
+    username = request.args.get('username')
+    password = request.args.get('password')
+
+    with open('config.json') as data_file:
+        data = json.load(data_file)
+        if username == data["username"] and password == data["password"]:
+            pins = [LED(12), LED(16), LED(20), LED(21)]
+            for pin in pins:
+                pin.off()
+            sequence = [[1, 0, 0, 0],
+                        [1, 1, 0, 0],
+                        [0, 1, 0, 0],
+                        [0, 1, 1, 0],
+                        [0, 0, 1, 0],
+                        [0, 0, 1, 1],
+                        [0, 0, 0, 1],
+                        [1, 0, 0, 1]];
+
+            for i in range(0, 32):
+                for halfstep in range(8, 0):
+                    for pin in range(0, len(pins)):
+                        if sequence[halfstep][pin] == 1:
+                            pins[pin].on()
+                        else:
+                            pins[pin].off()
+
+                    time.sleep(.001)
+
+            for pin in pins:
+                pin.off()
+                print "Setting pin low"
+        else:
+            return ""
+
+    with open('config.json', 'w') as data_file:
+        data["horizontal_orientation"] = int(data["horizontal_orientation"]) - 1
+        data_file.write(json.dumps(data))
+
+        return ""
+
     return ""
 def step_forward():
+    username = request.args.get('username')
+    password = request.args.get('password')
+
+    with open('config.json') as data_file:
+        data = json.load(data_file)
+        if username == data["username"] and password == data["password"] and int(data["vertical_orientation"]) < 8:
+
+            pins = [LED(6), LED(13), LED(19), LED(26)]
+            for pin in pins:
+                pin.off()
+            sequence = [[1, 0, 0, 0],
+                        [1, 1, 0, 0],
+                        [0, 1, 0, 0],
+                        [0, 1, 1, 0],
+                        [0, 0, 1, 0],
+                        [0, 0, 1, 1],
+                        [0, 0, 0, 1],
+                        [1, 0, 0, 1]];
+
+            for i in range(0, 32):
+                for halfstep in range(0, 8):
+                    for pin in range(0, len(pins)):
+                        if sequence[halfstep][pin] == 1:
+                            pins[pin].on()
+                        else:
+                            pins[pin].off()
+
+                    time.sleep(.001)
+
+            for pin in pins:
+                pin.off()
+                print "Setting pin low"
+        else:
+            return ""
+    with open('config.json', 'w') as data_file:
+        data["vertical_orientation"] = int(data["vertical_orientation"]) + 1
+        data_file.write(json.dumps(data))
+
+        return ""
+
     return ""
 def step_backwards():
+    username = request.args.get('username')
+    password = request.args.get('password')
+
+    with open('config.json') as data_file:
+        data = json.load(data_file)
+        if username == data["username"] and password == data["password"] and int(data["vertical_orientation"]) > 0:
+            pins = [LED(6), LED(13), LED(19), LED(26)]
+            for pin in pins:
+                pin.off()
+            sequence = [[1, 0, 0, 0],
+                        [1, 1, 0, 0],
+                        [0, 1, 0, 0],
+                        [0, 1, 1, 0],
+                        [0, 0, 1, 0],
+                        [0, 0, 1, 1],
+                        [0, 0, 0, 1],
+                        [1, 0, 0, 1]];
+
+            for i in range(0, 32):
+                for halfstep in range(8, 0):
+                    for pin in range(0, len(pins)):
+                        if sequence[halfstep][pin] == 1:
+                            pins[pin].on()
+                        else:
+                            pins[pin].off()
+
+                    time.sleep(.001)
+
+            for pin in pins:
+                pin.off()
+                print "Setting pin low"
+        else:
+            return ""
+
+    with open('config.json', 'w') as data_file:
+        data["vertical_orientation"] = int(data["vertical_orientation"]) - 1
+        data_file.write(json.dumps(data))
+
+        return ""
+
     return ""
 
 
